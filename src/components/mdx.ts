a886ee4ecfce0969c6f97a1313e81685101e9e73
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-11-10 21:27:29 -0500

    reorder imports

--- src/components/mdx.ts
+++ src/components/mdx.ts
@@ -1,13 +1,13 @@
-import Link from '@/components/ui/Link.astro';
 import H1 from '@/components/ui/H1.astro';
 import H2 from '@/components/ui/H2.astro';
 import HrDots from '@/components/ui/HrDots.astro';
+import Link from '@/components/ui/Link.astro';
 
 export const mdxComponents = {
   a: Link,
   h1: H1,
   h2: H2,
-  hr: HrDots
+  hr: HrDots,
 };
 
 export default mdxComponents;
